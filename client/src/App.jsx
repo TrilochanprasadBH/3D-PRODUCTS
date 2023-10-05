251c40869a3e3df3a07448d3c3e0d4c2f5772ad3
TrilochanprasadBH <104064013+TrilochanprasadBH@users.noreply.github.com>
2023-10-05 14:24:37 +0530

    pages and canvas , imported into app basic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
-
-import './App.css'
+import Home from "./pages/Home"
+import Customiser from "./pages/Customiser"
+import Canvas from "./canvas"
 
 function App() {
   
 
   return (
-    <>
-      <h1>hey</h1>
-    </>
+    <main className="app transition-all ease-in">
+      {/* <p className='head-text'>Three JS</p> */}
+      {/* we are using tailwind css hence classname = properties here act as css code, head-text gives big text */}
+      <Home/>
+      <Canvas/>
+      <Customiser/>
+    </main>
   )
 }
 
---
+++ client/src/canvas/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//file name is index.jsx, i is not capital as this is index.jsx, not some other component name 
+//our function inside can have diff name  , here it is canvas 
+
+
+const Canvas = () => {
+  return (
+    <div>Canvas</div>
+  )
+}
+
+export default Canvas
